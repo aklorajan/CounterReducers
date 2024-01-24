70b108eb6113605bc5847b09b0e01fc2e863e95a
aklor <aklorajan@yahoo.com>
2024-01-25 01:03:51 +0330

    first commit

---
+++ src/app/store/counter.reducer.ts
@@ -0,0 +1,12 @@
+import {createReducer} from "@ngrx/store";
+
+
+const initialState = 0;
+
+// const counterReduce = createReducer(
+//   initialState
+// );
+ export function counterReducer(state = initialState){
+   return state;
+}
+
